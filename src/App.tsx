6416fcc4aff687712809340020a9781378a22095
Sharon Craddock <sharoncraddock.tech@gmail.com>
2023-07-18 08:04:14 -0600

    Initial setup.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1>Initial App</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '7': '4rem',
+        '8': '5rem',
+        '9': '6rem',
+        '10': '7rem',
+        '11': '8rem',
+        '12': '9rem',
+        '13': '10rem',
+      },
+      lineHeight: {
+        '11': '3.5rem',
+      },
+      colors: {
+        'custom-blue': '#0a192f',
+      },
+    },
+    fontFamily: {
+      'sans': ['Inter', 'ui-sans-serif', 'system-ui'],
+      'mono': ['Inconsolata', 'SFMono-Regular'],
+      'split-flap': ['Oswald', 'ui-sans-serif'],
+    },
+  },
+  plugins: [],
+}
+
